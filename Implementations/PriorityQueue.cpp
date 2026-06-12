06409ac368103d4282153ab1ffaf93aafe828705
Ashutosh Kumar <ak956901@gmail.com>
2026-06-12 16:54:25 +0000

    heap

--- Implementations/PriorityQueue.cpp
+++ Implementations/PriorityQueue.cpp
@@ -2,18 +2,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class PriorityQueue{
-    public:
+// Min Heap
+class PriorityQueue
+{
 
-    // int* arr;
+    vector<int> heap;
 
-    // PriortiyQueue(){
-    //     arr=new int[]
-    // }
+public:
+    void insert(int v)
+    {
+        heap.push_back(v);
+        int i = heap.size() - 1;
 
+        while (i > 0)
+        {
+            int parent = (i - 1) / 2;
+            if (heap[parent] <= heap[i])
+                break;
+            swap(heap[parent], heap[i]);
+            i = parent;
+        }
+    }
 
+    void heapify(int i)
+    {
+        int smallest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        int size = heap.size();
+        if (left < size && heap[left] < heap[smallest])
+        {
+            smallest = left;
+        }
+        if (right < size && heap[right] < heap[smallest])
+        {
+            smallest = right;
+        }
+
+        if (smallest != i)
+        {
+            swap(heap[smallest], heap[i]);
+            heapify(smallest);
+        }
+    }
+
+    int extractMin()
+    {
+        int temp = heap[0];
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapify(0);
+        return temp;
+    }
 };
-int main(){
-    
+int main()
+{
+    PriorityQueue pq;
+    pq.insert(5);
+    pq.insert(1);
+    pq.insert(-10);
+    pq.insert(26);
+    pq.insert(15);
+    pq.insert(35);
+
+    cout << "Minimum element:" << pq.extractMin() << endl;
+    cout << "Minimum element:" << pq.extractMin() << endl;
+    cout << "Minimum element:" << pq.extractMin() << endl;
+    cout << "Minimum element:" << pq.extractMin() << endl;
+    cout << "Minimum element:" << pq.extractMin() << endl;
+
     return 0;
 }
